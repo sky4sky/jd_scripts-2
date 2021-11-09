7915b0d8f8c8c2fe2e63460304b2f070efa88f66
wisz2021 <wei09mozzie@gmail.com>
2021-11-09 09:42:15 +0800

    赚京豆-瓜分京豆活动又上线了。移除过期活动

--- activity/jd_champion_game.js
+++ activity/jd_champion_game.js
@@ -9,9 +9,9 @@ const $ = new Env('电竞预言家');
 //设置选的答案
 const rightAnswerCode = 'A';
 $.rightAnswerCode = process.env.rightAnswerCode ? process.env.rightAnswerCode : rightAnswerCode;
-const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = $.isNode() ? require('../sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 
@@ -436,7 +436,7 @@ function toHelp(code = "") {
 }
 function updateShareCodesCDN(url = 'https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/champion_game.json') {
   return new Promise(resolve => {
-    $.get({url , headers:{"User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")}, timeout: 200000}, async (err, resp, data) => {
+    $.get({url , headers:{"User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('../USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")}, timeout: 200000}, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -48,8 +48,6 @@
 0 0-23/6 * * * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
 # 京东超级盒子（2021年10月25日～2021年11月11日）
 0 0,2,21,22 * * * node /scripts/jd_super_box.js >> /scripts/logs/jd_super_box.log 2>&1
-# 电竞预言家（2021年10月22日～2021年11月6日）
-0 0,11,15,17 * * * node /scripts/jd_champion_game.js >> /scripts/logs/jd_champion_game.log 2>&1
 # 限时抢京豆（2021-10-20到2021-11-20）
 23 2,3 * * * node /scripts/jd_xsqjd.js >> /scripts/logs/jd_xsqjd.log 2>&1
 # 京东双十一京享红包
