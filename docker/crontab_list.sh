d2b17e2285a8f4f88756c2008044562622f63f86
DoveBoy <393366046@qq.com>
2021-11-13 09:40:35 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -47,7 +47,7 @@
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
-# 东东超市兑换奖品
+# 京东签到图形验证
 5 0,20 * * * node /scripts/jd_sign.js >> /scripts/logs/jd_sign.log 2>&1
 # 东东超市兑换奖品
 0,30 0 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
